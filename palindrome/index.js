1b5a827c0dd9298923545840c30cce8bf041b3f4
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-10-28 13:03:14 +0100

    Updated palindromes

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+
+//   return str === reversed;
+// }
 
 module.exports = palindrome;
--- reversestring/index.js
+++ reversestring/index.js
@@ -2,12 +2,28 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reverse().join('');
+  let reversed = '';
+
+  for (const character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
 }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+// reverse('asdf');
 
 module.exports = reverse;
