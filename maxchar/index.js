b54c5725bc82ad3b1c557d038d3e5373a0390cb4
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-02 09:43:34 +0100

    Updated with maxChar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (const char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (const char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
