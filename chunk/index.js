2304e94003494b536c0a50b0f17b51330da52841
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-04 11:14:37 +0100

    Updated with chunk algorithm

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,20 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+
+  for (const element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
