5da743295cef71279142fbbf5adabd06e3c75aae
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-10 00:19:50 +0100

    Steps algorithm

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//~~~~~~Recursive approach~~~~~~~~~//
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
 
 module.exports = steps;
