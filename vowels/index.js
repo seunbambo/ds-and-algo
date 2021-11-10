85114ef17b5969335427121333831652c3d80103
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-10 12:51:36 +0100

    Vowel algorithm

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (const char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
 
 module.exports = vowels;
