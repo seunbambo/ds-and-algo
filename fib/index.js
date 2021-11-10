ac5ad6ec78cee2e458e89004378ef8ecd652d509
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-10 16:54:06 +0100

    Fibonacci algorithm

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Recursive approah
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
 
 module.exports = fib;
