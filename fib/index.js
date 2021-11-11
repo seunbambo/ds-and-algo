e5e5c43c4e52ad68d2580ce47eb965372d9fe10e
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-11-11 03:50:29 +0100

    Memoized fib algorithm

--- fib/index.js
+++ fib/index.js
@@ -8,14 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-// Recursive approah
-function fib(n) {
+// Recursive approach
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 // function fib(n) {
 //   const result = [0, 1];
